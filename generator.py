0c043a92307e0a7b2d6b8142ba0fffecbfdba3cd
Viktor <99266266+ViktorSky@users.noreply.github.com>
2023-03-26 23:35:47 -0400

    Update generator.py

--- generator.py
+++ generator.py
@@ -6,7 +6,7 @@
 parameters = {
 
     "community-link":
-        "http://aminoapps.com/invite/88KX3AEDHT",
+        "http://aminoapps.com/invite/77FC1LEDHT",
     "proxies": {
         "https": None #"159.197.250.171:3128",
     }
